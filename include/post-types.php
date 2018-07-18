c062c339e39c6925c152a784d6616b27cea9290e
Kyle Robertson <kyle@typefirst.ca>
2018-07-17 23:40:16 -0700

    Move custom post types into their own directory.

--- include/post-types.php
+++ include/post-types.php
@@ -7,28 +7,4 @@
  *
  */
 
-register_post_type( 'joist_example', array(
-	'labels'          => array(
-		'name'               => __( 'Examples', 'joist' ),
-		'singular_name'      => __( 'Example', 'joist' ),
-		'add_new_item'       => __( 'Add example', 'joist' ),
-		'view_item'          => __( 'View example', 'joist' ),
-		'edit_item'          => __( 'Edit example', 'joist' ),
-		'new_item'           => __( 'New example', 'joist' ),
-		'search_items'       => __( 'Search examples', 'joist' ),
-		'not_found'          => __( 'No examples found.', 'joist' ),
-		'not_found_in_trash' => __( 'No examples found in trash.', 'joist' ),
-	),
-	'public'          => true,
-	'show_ui'         => true,
-	'capability_type' => 'post',
-	'menu_icon'       => 'dashicons-media-text',
-	'rewrite'         => array(
-		'slug' => 'examples',
-	),
-	'supports'        => array(
-		'title',
-		'editor',
-		'thumbnail',
-	),
-) );
+require_once( 'post_types/example.php' );
