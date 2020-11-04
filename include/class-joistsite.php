0a1ca32d9825bc813d59e2881f996f7dd216c24b
Kyle Robertson <kyle@typefirst.ca>
2020-11-04 13:56:25 -0800

    Remove legacy support for emojis.

--- include/class-joistsite.php
+++ include/class-joistsite.php
@@ -67,6 +67,8 @@ class JoistSite extends TimberSite {
 		// Add custom roles and capabilities
 		$this->register_user_roles();
 
+		add_action( 'init', [ $this, 'disable_emojis' ] );
+
 		parent::__construct();
 	}
 
@@ -236,5 +238,47 @@ class JoistSite extends TimberSite {
 		include_once( __DIR__ . '/widget-areas.php' );
 
 		include_once( __DIR__ . '/widgets/register.php' );
+	}
+
+	// From https://kinsta.com/knowledgebase/disable-emojis-wordpress/#2-disable-emojis-in-wordpress-with-code
+	public function disable_emojis() {
+		remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+		remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+		remove_action( 'wp_print_styles', 'print_emoji_styles' );
+		remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
+		remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+		remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+		remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+
+		add_filter(
+			'tiny_mce_plugins',
+			function ( $plugins ) {
+				if ( is_array( $plugins ) ) {
+					return array_diff( $plugins, array( 'wpemoji' ) );
+				} else {
+					return array();
+				}
+			}
+		);
+
+		add_filter(
+			'wp_resource_hints',
+			function ( $urls, $relation_type ) {
+				if ( 'dns-prefetch' === $relation_type ) {
+					// This filter is documented in wp-includes/formatting.php.
+					$emoji_svg_url = apply_filters(
+						'emoji_svg_url',
+						'https://s.w.org/images/core/emoji/2/svg/'
+					);
+
+					$urls = array_diff( $urls, array( $emoji_svg_url ) );
+				}
+
+				return $urls;
+			},
+			10,
+			2
+		);
 	}
 }
